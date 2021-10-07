2c8a8b936d27ec6ca6a9909126ffceffb156daa5
Kas Barber <kaseanbarber02@gmail.com>
2021-10-07 10:00:18 -0400

    added a tolower

--- Program.cs
+++ Program.cs
@@ -39,24 +39,24 @@ namespace Lab8
                 }
                 Console.WriteLine(DiceRoller(numGiven));
 
-                    while (true)
+                while (true)
+                {
+                    Console.WriteLine("Do you want to try again? yes or no");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() == "no")
                     {
-                        Console.WriteLine("Do you want to try again? yes or no");
-                        string answer = Console.ReadLine();
-                        if (answer.ToLower() == "no")
-                        {
-                            keepLooping = false;
-                            break;
-                        }
-                        else if (answer != "yes")
-                        {
-                            Console.WriteLine("That was not a valid option");
-                        }
+                        keepLooping = false;
+                        break;
+                    }
+                    else if (answer != "yes")
+                    {
+                        Console.WriteLine("That was not a valid option");
+                    }
                     else
                     {
                         break;
                     }
-                    }
+                }
             }
             Console.ReadKey();
         }
@@ -69,7 +69,7 @@ namespace Lab8
             int die1 = rand.Next(1, diceSides + 1);
             int die2 = rand.Next(1, diceSides + 1);
             int total = die1 + die2;
-            string res="";
+            string res = "";
 
             if (diceSides != 6)
             {
@@ -79,28 +79,28 @@ namespace Lab8
             {
                 if (die2 == 1 && die1 == 1)
                 {
-                    res+="You rolled 2 1s. You got snake eyes. ";
+                    res += "You rolled 2 1s. You got snake eyes. ";
                 }
                 else if ((die1 == 1 && die2 == 2) || (die1 == 2 && die2 == 1))
                 {
-                    res+="You rolled a 1 and a 2. You got ace deuce. ";
+                    res += "You rolled a 1 and a 2. You got ace deuce. ";
                 }
                 else if (die1 == 6 && die2 == 6)
                 {
-                    res+="You rolled 2 6s. You got box cars. ";
+                    res += "You rolled 2 6s. You got box cars. ";
                 }
                 else
                 {
-                    res+=$"You rolled a {die1} and a {die2}. Your total is {total}. ";
+                    res += $"You rolled a {die1} and a {die2}. Your total is {total}. ";
                 }
 
                 if (total == 2 || total == 3 || total == 12)
                 {
-                    res+="You got craps. Unfortunate. ";
+                    res += "You got craps. Unfortunate. ";
                 }
                 else if (total == 7 || total == 11)
                 {
-                    res+="You won, way to go.";
+                    res += "You won, way to go.";
                 }
             }
             return res;
